844d927a6622ed5b7a945e4baa2e124f5f66591d
index01d <indevelope@gmail.com>
2018-08-31 18:14:09 +0500

    [fix] pm2

--- pm2.config.js
+++ pm2.config.js
@@ -5,5 +5,16 @@ module.exports = {
     cwd: '/home/eshop/eshop_api/current',
     instances: 2,
     exec_mode: 'cluster'
-  }]
+  }],
+  deploy: {
+    production: {
+      user: 'eshop',
+      host: ['185.4.74.175'],
+      ref: 'origin/master',
+      repo: 'git@github.com:indevelope/eshopAPI.git',
+      'post-deploy': 'npm install && pm2 startOrRestart pm2.config.js --env production',
+      "path" : "/home/eshop/eshop_api/current",
+      "ssh_options": "StrictHostKeyChecking=no"
+    }
+  }
 }
